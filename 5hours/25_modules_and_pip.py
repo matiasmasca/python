eb4a232f3dcc73008feca8511b96c6ff0a7c7356
Matias Mascazzini <matiasmasca@gmail.com>
2019-12-26 07:12:13 -0300

    modulos

--- 5hours/25_modules_and_pip.py
+++ 5hours/25_modules_and_pip.py
@@ -1,7 +1,26 @@
 import herramientas_utiles
 
 # sin la extension,
+# ojo con el nombre, con numeros al principio no funciona.
 # un modulo es un archivo de python qu epodemos invocar o importar en nuestro codigo. para tener acceso a esas funciones.
 # evitemos el copy and paste...
 
 print(herramientas_utiles.roll_dice(10))
+
+# Modulos en python.
+# https://docs.python.org/3/py-modindex.html
+# depende de la version
+# estos son modulos que vienen en python y que los podemos importar cuando quieras.
+# en el listado lo que no tengan una URL de donde estan ubicados son los que se usan directamente, los otros hay que ubircar donde estan en tu maquina.
+
+# podemos guardar los modulos en una carpeta llamada "lib".
+
+# build-in modules.
+# external modules.
+
+
+# https://youtu.be/rfscVS0vtbw?t=13024
+
+# PIP
+# es una herramienta para administrar paquetes, o librerias.
+#
